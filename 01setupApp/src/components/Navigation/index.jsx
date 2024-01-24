6fef1539e8d5ff7703dcea34682cedf671cd58a5
Kalal mukesh <105076417+Mukeshkalal@users.noreply.github.com>
2024-01-24 11:25:23 +0530

    update

--- 01setupApp/src/components/Navigation/index.jsx
+++ 01setupApp/src/components/Navigation/index.jsx
@@ -1,7 +1,13 @@
+// import { useEffect } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 function Navigation() {
+    // const [isLoggedIn, setIsLoggedIn]=useState(true)
+    const handleClear = () => {
+        localStorage.clear()
+        // setIsLoggedIn(false)
+    }
 
     return (
         <Navbar expand="lg" bg="dark" data-bs-theme="dark">
@@ -17,7 +23,11 @@ function Navigation() {
                         <NavLink to="/" className='nav-link'>Home</NavLink>
                         <NavLink to="/contact" className='nav-link'>Contact US</NavLink>
                         <NavLink to="/register" className='nav-link'>Register</NavLink>
-                        <NavLink to="/" className='nav-link'>Logout</NavLink>
+                        {/* {
+                            isLoggedIn?<button className='nav-link' onClick={handleClear}>Logout</button>:
+                            <NavLink to="/login" className='nav-link' onClick={()=>{setIsLoggedIn(false)}}>Login</NavLink>
+                        } */}
+                        <button className='nav-link' onClick={handleClear}>Logout</button>
                         <NavLink to="/login" className='nav-link'>Login</NavLink>
                         <NavLink to="/exams" className='nav-link'>Exams</NavLink>
                     </Nav>
--- 01setupApp/src/pages/Exams/index.jsx
+++ 01setupApp/src/pages/Exams/index.jsx
@@ -29,6 +29,7 @@ function Exams() {
             setIsLoginTo(true);
             callApi();
         }
+
     }, [])
 
     return (
